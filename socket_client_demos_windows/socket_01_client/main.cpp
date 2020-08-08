20de3fe2d4f9f7ac1153f341ea2d67b3699a04b6
QwQhai <w_qhai@yeah.net>
2020-08-08 19:45:18 +0800

    一对一聊天

--- socket_client_demos_windows/socket_01_client/main.cpp
+++ socket_client_demos_windows/socket_01_client/main.cpp
@@ -4,8 +4,11 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include <thread>
 
-int __cdecl wmain(int argc, char** argv) {
+void do_recv(int conn, sockaddr_in& peer);
+
+int main(int argc, char** argv) {
     //-----------------------------------------
     // Declare and initialize variables
     WSADATA wsaData = { 0 };
@@ -26,18 +29,28 @@ int __cdecl wmain(int argc, char** argv) {
         std::cerr << "connect faild" << std::endl;
     }
 
-    char send_buf[1024];
-    char recv_buf[1024];
 
+    std::thread recv_thread(do_recv, sock, std::ref(serve_addr));
+    recv_thread.detach();
+
+    char send_buf[1024];
     while (std::cin.getline(send_buf, 1024)) {
-        memset(recv_buf, 0, sizeof(recv_buf));
         send(sock, send_buf, strlen(send_buf), 0);
-        recv(sock, recv_buf, sizeof(recv_buf), 0);
-        std::cout << "recv: " << recv_buf << std::endl;
     }
 
     closesocket(sock);
     WSACleanup();
 
     return 0;
+}
+
+void do_recv(int conn, sockaddr_in& peer) {
+    char recv_buf[1024] = { 0 };
+    char ip[INET_ADDRSTRLEN];
+    int recv_len;
+    while ((recv_len = recv(conn, recv_buf, sizeof(recv_buf), 0)) > 0) {
+        std::cout << "[" << inet_ntop(AF_INET, &peer.sin_addr.s_addr, ip, sizeof(ip)) << ":" << ntohs(peer.sin_port) << "]:\t"
+            << recv_buf << std::endl;
+        memset(recv_buf, 0, sizeof(recv_buf));
+    }
 }
