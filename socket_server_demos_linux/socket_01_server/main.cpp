e7b286a8b8d2ac63a44330116963057ddb8c6ab1
QwQhai <w_qhai@yeah.net>
2020-08-08 15:37:41 +0800

    一对一简易server

--- socket_server_demos_linux/socket_01_server/main.cpp
+++ socket_server_demos_linux/socket_01_server/main.cpp
@@ -1,43 +1,48 @@
-﻿// link with Ws2_32.lib
-#pragma comment(lib,"Ws2_32.lib")
+﻿#include <iostream>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <iostream>
+int main(int argc, char** argv) {
 
-int __cdecl wmain(int argc, char** argv) {
-    //-----------------------------------------
-    // Declare and initialize variables
-    WSADATA wsaData = { 0 };
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData)) {
-        std::cerr << "WSAStartup WSAStartup " << std::endl;
-        return 1;
-    }
-
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
     sockaddr_in serve_addr;
     memset(&serve_addr, 0, sizeof(serve_addr));
     serve_addr.sin_family = AF_INET;
     serve_addr.sin_port = htons(5188);
-    inet_pton(AF_INET, "127.0.0.1", &serve_addr.sin_addr.s_addr); /*server ip*/
+    inet_pton(AF_INET, "0.0.0.0", &serve_addr.sin_addr.s_addr); /*server ip*/
 
-    if (connect(sock, reinterpret_cast<sockaddr*>(&serve_addr), sizeof(serve_addr)) < 0) {
-        std::cerr << "connect faild" << std::endl;
-    }
+    int on = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    bind(sock, reinterpret_cast<sockaddr*>(&serve_addr), sizeof(serve_addr));
+    listen(sock, SOMAXCONN);
 
-    char send_buf[1024];
-    char recv_buf[1024];
+    sockaddr_in peer_addr;
+    socklen_t peer_len = sizeof(peer_addr);
 
-    while (std::cin.getline(send_buf, 1024)) {
+    int conn = accept(sock, reinterpret_cast<sockaddr*>(&peer_addr), &peer_len);
+    char recv_buf[1024] = { 0 };
+    char ip[INET_ADDRSTRLEN];
+
+    std::cout << inet_ntop(AF_INET, &peer_addr.sin_addr.s_addr, ip, sizeof(ip)) << ":" << ntohs(peer_addr.sin_port) << ": " << std::endl;
+    while (true) {
+        int recv_len = recv(conn, recv_buf, sizeof(recv_buf), 0);
+        std::cout << recv_len << std::endl;
+        if (recv_len <= 0) {
+            std::cout << "closed connection." << std::endl;
+            break;
+        }
+        else {
+            std::cout << recv_buf << std::endl;
+        }
+        send(conn, recv_buf, recv_len, 0);
         memset(recv_buf, 0, sizeof(recv_buf));
-        send(sock, send_buf, strlen(send_buf), 0);
-        recv(sock, recv_buf, sizeof(recv_buf), 0);
-        std::cout << "recv: " << recv_buf << std::endl;
     }
 
-    closesocket(sock);
-    WSACleanup();
-
+    close(conn);
+    close(sock);
     return 0;
 }
